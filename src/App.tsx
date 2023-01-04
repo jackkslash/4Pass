97bad3476901b7ff708b2cdba2ceb0baae68afff
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-04 22:41:22 +0000

    Added home page with design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './index.css'
 
 function App() {
   return (
-    <div className=" flex justify-center h-screen text-3xl font-bold min-w-[400px] min-h-[400px]">
-      <p>4Pass</p>
+    <div className="flex flex-col items-center justify-center h-screen font-bold min-w-[400px] min-h-[600px] space-y-24">
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='pt-4 text-white text-xl'>4Pass</h1>
+      <p className='pt-4'>correct horse battery staple</p>
+      <p>your passwords.</p>
+      </div>
+      <div>
+        <button className='btn btn-wide'>Create your phrase</button>
+      </div>
+      
     </div>
   )
 }
