dc9947532051bd22302ed34687e07c1e4948b028
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-06 23:51:05 +0000

    Designed into input page, added route to main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Navigation, Routes, Route, HashRouter } from 'react-rout
 import Home from './pages/home'
 import Intro from './pages/intro'
 import './index.css'
+import Main from './pages/main'
 
 
 function App() {
   return (
-    <div className='min-w-[400px] min-h-[600px] space-y-24'>
+    <div className='min-w-[400px] min-h-[600px]'>
       <HashRouter>
         <Routes>
          <Route path="/" element={<Home />}/>
          <Route path="/intro" element={<Intro />}/>
+         <Route path="/main" element={<Main/>}/>
          <Route/>
         </Routes>
       </HashRouter>
--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const intro = () => {
   return (
-    <div className='flex flex-col items-center justify-center text-white text-xl'>intro</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='pb-4'>
+        <div className='text-white text-xl'>Input four random keywords.</div>
+      </div>
+      <div className='flex flex-col items-center justify-center space-y-2.5'>
+        <input type="text" placeholder="Phrase one" className="input input-bordered input-primary placeholder:text-center" />
+        <input type="text" placeholder="Phrase two" className="input input-bordered input-primary placeholder:text-center" />
+        <input type="text" placeholder="Phrase three" className="input input-bordered input-primary placeholder:text-center" />
+        <input type="text" placeholder="Phrase four" className="input input-bordered input-primary placeholder:text-center" />
+        <div className='pt-4'>
+          <Link to={'/main'} className='btn btn-wide'>Submit</Link>
+        </div>
+      </div>
+
+    </div>
+
   )
 }
 
