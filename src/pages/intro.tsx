bc555a6ea2546c1f5a002a3c2ef45f2a27c44b55
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-13 23:14:40 +0000

    Added options to settings page and changes text field colour

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -5,13 +5,13 @@ const intro = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <div className='pb-4'>
-        <div className='text-white text-xl'>Input four random keywords.</div>
+        <div className='text-white text-xl'>Input four random keywords to be your phrase.</div>
       </div>
       <div className='flex flex-col items-center justify-center space-y-2.5'>
-        <input type="text" placeholder="Phrase one" className="input input-bordered input-primary placeholder:text-center" />
-        <input type="text" placeholder="Phrase two" className="input input-bordered input-primary placeholder:text-center" />
-        <input type="text" placeholder="Phrase three" className="input input-bordered input-primary placeholder:text-center" />
-        <input type="text" placeholder="Phrase four" className="input input-bordered input-primary placeholder:text-center" />
+        <input type="text" placeholder="Phrase one" className="input input-bordered placeholder:text-center" />
+        <input type="text" placeholder="Phrase two" className="input input-bordered placeholder:text-center" />
+        <input type="text" placeholder="Phrase three" className="input input-bordered placeholder:text-center" />
+        <input type="text" placeholder="Phrase four" className="input input-bordered placeholder:text-center" />
         <div className='pt-4'>
           <Link to={'/main'} className='btn btn-wide'>Submit</Link>
         </div>
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -6,10 +6,20 @@ const settings = () => {
   return (
     <div className='flex flex-col items-center h-screen'>
       <div className='flex flex-row justify-center items-center h-1/5 pt-4 space-x-4'>
-        <div className='text-white text-xl font-bold'>Passwords</div>
+        <div className='text-white text-xl font-bold'>Settings</div>
         <div className=''>
           <Link to={'/main'} className='btn btn-circle btn-sm'><XMarkIcon className="h-4 w-4 text-blue-500" /></Link>
         </div>
+      </div>
+      <div className='flex flex-col text-xl space-y-4'>
+        <div className='flex flex-row btn space-x-2'>
+          <div>Clear salts</div>
+          <XMarkIcon className="h-4 w-4 text-white-500" />
+        </div>
+        <button className='flex flex-row btn space-x-2'>
+          <div>Wipe data</div>
+          <XMarkIcon className="h-4 w-4 text-white-500" />
+        </button>
       </div>
     </div>
   )
