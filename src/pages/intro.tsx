ab569241c0dc31661fae81e828f6c70fe057d2b4
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-15 23:54:40 +0000

    Added checkpoint check for returning users

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const intro = () => {
+
+
+  const handleSubmit = () => {
+    localStorage.setItem('checkPoint', "true");
+  }
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <div className='pb-4'>
         <div className='text-white text-xl'>Input four random keywords to be your phrase.</div>
       </div>
-      <div className='flex flex-col items-center justify-center space-y-2.5'>
+      <form  className='flex flex-col items-center justify-center space-y-2.5'>
         <input type="text" placeholder="Phrase one" className="input input-bordered placeholder:text-center" />
         <input type="text" placeholder="Phrase two" className="input input-bordered placeholder:text-center" />
         <input type="text" placeholder="Phrase three" className="input input-bordered placeholder:text-center" />
         <input type="text" placeholder="Phrase four" className="input input-bordered placeholder:text-center" />
         <div className='pt-4'>
-          <Link to={'/main'} className='btn btn-wide'>Submit</Link>
+          <Link to={'/main'} className='btn btn-wide' onClick={handleSubmit}>Submit</Link>
         </div>
-      </div>
+      </form>
 
     </div>
 
