7fcde866c5ff74e80841ac223a3049c32a69b4e2
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-16 23:11:05 +0000

    Added saving of form data to local

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const intro = () => {
+  const navigate = useNavigate();
 
+  const handleSubmit = (event: any) => {
+    console.log('handleSubmit ran');
+    event.preventDefault(); // 👈️ prevent page refresh
 
-  const handleSubmit = () => {
-    localStorage.setItem('checkPoint', "true");
+    const phrase = {
+      one: event.target.one.value,
+      two:event.target.two.value,
+      three:event.target.three.value,
+      four:event.target.four.value
+    }
+    // 👇️ access input values here
+    console.log(phrase);
+    localStorage.setItem('checkPoint', 'true')
+    localStorage.setItem('phrase', JSON.stringify(phrase))
+    navigate('/main')
   }
+  console.log('test');
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <div className='pb-4'>
         <div className='text-white text-xl'>Input four random keywords to be your phrase.</div>
       </div>
-      <form  className='flex flex-col items-center justify-center space-y-2.5'>
-        <input type="text" placeholder="Phrase one" className="input input-bordered placeholder:text-center" />
-        <input type="text" placeholder="Phrase two" className="input input-bordered placeholder:text-center" />
-        <input type="text" placeholder="Phrase three" className="input input-bordered placeholder:text-center" />
-        <input type="text" placeholder="Phrase four" className="input input-bordered placeholder:text-center" />
-        <div className='pt-4'>
-          <Link to={'/main'} className='btn btn-wide' onClick={handleSubmit}>Submit</Link>
-        </div>
+      <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center space-y-2.5'>
+        <input id='one' name='one' type="text" placeholder="Phrase one" className="input input-bordered placeholder:text-center" />
+        <input id='two' name='two' type="text" placeholder="Phrase two" className="input input-bordered placeholder:text-center" />
+        <input id='three' name='three' type="text" placeholder="Phrase three" className="input input-bordered placeholder:text-center" />
+        <input id='four' name='four' type="text" placeholder="Phrase four" className="input input-bordered placeholder:text-center" />
+        <button className='btn' type='submit'>Submit</button>
       </form>
 
     </div>
