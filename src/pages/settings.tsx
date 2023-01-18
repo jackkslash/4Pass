07a6a273438fcc754b22d28b634e0161432d1118
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-18 21:56:39 +0000

    Added clear and wipe functions

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -3,6 +3,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const settings = () => {
+
+  const wipeData = () =>{
+    localStorage.clear();
+  }
+
+  const clearSalts = () => {
+    localStorage.setItem("salts", "")
+  }
+
   return (
     <div className='flex flex-col items-center h-screen'>
       <div className='flex flex-row justify-center items-center h-1/5 pt-4 space-x-4'>
@@ -12,11 +21,11 @@ const settings = () => {
         </div>
       </div>
       <div className='flex flex-col text-xl space-y-4'>
-        <div className='flex flex-row btn space-x-2'>
+        <button className='flex flex-row btn space-x-2' onClick={clearSalts}>
           <div>Clear salts</div>
           <XMarkIcon className="h-4 w-4 text-white-500" />
-        </div>
-        <button className='flex flex-row btn space-x-2'>
+        </button>
+        <button className='flex flex-row btn space-x-2' onClick={wipeData}>
           <div>Wipe data</div>
           <XMarkIcon className="h-4 w-4 text-white-500" />
         </button>
