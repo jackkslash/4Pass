e2b251fbd3e8fbc1cfa95ca4dc9eb18bdf8e37db
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-01-21 16:26:23 +0000

    Updated styles and added phrase output to main

--- src/components/salter.tsx
+++ src/components/salter.tsx
@@ -1,13 +1,16 @@
 import { DocumentDuplicateIcon, XMarkIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
-const Salter = (saltText:any) => {
+const Salter = (saltText: any) => {
 
   return (
-    <div className='flex flex-row justify-center space-x-4 pb-2'>
-    <div className='flex justify-center items-center pl-2'>{saltText.saltText}</div>
-    <div className='btn' onClick={() => {navigator.clipboard.writeText(saltText.saltText)}}><DocumentDuplicateIcon className="h-4 w-4 text-blue-500" /></div>
-    <div className='btn'><XMarkIcon className="h-4 w-4 text-blue-500"/></div>
+    <div className='flex flex-row justify-center space-x-4 pb-2 w-full'>
+      <div className='flex justify-center items-center pl-2 w-1/2'>{saltText.saltText}</div>
+      <div className='w-1/2 space justify-center space-x-4 pb-2'>
+        <div className='btn' onClick={() => { navigator.clipboard.writeText(saltText.saltText) }}><DocumentDuplicateIcon className="h-4 w-4 text-blue-500" /></div>
+      <div className='btn '><XMarkIcon className="h-4 w-4 text-blue-500" /></div>
+      </div>
+      
     </div>
   )
 }
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,4 @@
-import { Cog8ToothIcon} from '@heroicons/react/24/solid'
+import { Cog8ToothIcon } from '@heroicons/react/24/solid'
 import { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import AddSalter from '../components/addSalter'
@@ -7,29 +7,36 @@ import Salter from "../components/salter"
 const main = () => {
   const alreadyUser = localStorage.getItem("checkPoint");
   const localSalts = localStorage.getItem("salts");
+  const phrase = JSON.parse(localStorage.getItem("phrase"));
   const navigate = useNavigate();
   const salts = localSalts.split(',');
- 
-  useEffect(()=>{
-    if(alreadyUser == null){navigate("/")}
 
-  },[])
+  useEffect(() => {
+    if (alreadyUser == null) { navigate("/") }
+
+  }, [])
   return (
     <div>
       <div className='flex flex-col items-center h-screen'>
-        <div className='flex flex-row justify-center items-center h-1/5 pt-4 space-x-4'>
-          <div className='text-white text-xl font-bold'>Passwords</div>
-          <div className=''>
-            <Link to={'/settings'} className='btn btn-circle btn-sm'><Cog8ToothIcon className="h-4 w-4 text-blue-500"/></Link>
+        <div className='flex flex-col justify-center items-center h-1/5 pt-4'>
+          <div className='flex flex-row space-x-4'>
+            <div className='text-white text-xl font-bold'>Passwords</div>
+            <div className=''>
+              <Link to={'/settings'} className='btn btn-circle btn-sm'><Cog8ToothIcon className="h-4 w-4 text-blue-500" /></Link>
+            </div>
+          </div>
+          <div className='pt-4'>
+            {phrase.one +" "+ phrase.two +" "+ phrase.three +" "+ phrase.four}
           </div>
         </div>
-        <div className='h-4/5'>
-          <AddSalter/>
-          {salts.map((m, index)=>(
-             <>
-             <Salter saltText={m}/>
-             </>
-             
+
+        <div className='h-4/5 w-1/3'>
+          <AddSalter />
+          {salts.map((m, index) => (
+            <>
+              <Salter saltText={m} />
+            </>
+
           ))}
         </div>
       </div>
